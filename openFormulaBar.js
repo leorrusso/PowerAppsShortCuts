ecc70e5955dd00be5c2eba52378d10185e0e7733
Leonardo Russo <62526027+leorrusso@users.noreply.github.com>
2023-07-19 12:17:54 -0300

    Create openFormulaBar.js

---
+++ openFormulaBar.js
@@ -0,0 +1,6 @@
+(function() {
+    'use strict';
+    let dropDown = document.getElementById("formula-bar-open-tray-button");
+    dropDown.setAttribute("accessKey","a");
+    // Your code here...
+})();
